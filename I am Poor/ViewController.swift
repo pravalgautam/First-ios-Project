f8c53563a6f287590118dcf40408f2b074fae3fb
Praval Gautam <pravalgautam@Pravals-MacBook-Air.local>
2022-12-26 03:05:35 +0530

    first Ios app

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Praval Gautam on 26/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGreen
+    }
+
+
+}
+
